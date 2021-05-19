929db1d007dff30e76a22bc9a9b75645d5e447a0
marcoguidara <marcoguidara@users.noreply.github.com>
2021-05-19 13:17:44 +0000

    Fix styling

--- src/QuickResponseCache.php
+++ src/QuickResponseCache.php
@@ -3,14 +3,13 @@
 namespace MarcoGuidara\QuickResponseCache;
 
 use Spatie\ResponseCache\Facades\ResponseCache;
-use Spatie\ResponseCache\ResponseCache as ResponseCacheResponseCache;
 
 class QuickResponseCache
 {
     public static function isEnabled(): bool
     {
         $config = config('quick-responsecache.enabled');
-        if (!is_bool($config)) {
+        if (! is_bool($config)) {
             return false;
         }
 
