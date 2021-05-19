75298ae9cab1b4bf90087819b042a048d9b5e833
Marco Guidara <marco.guidara@efesto.studio>
2021-05-19 12:11:55 +0200

    Add middleware to enable cache

---
+++ src/Middlewares/QuickCacheResponse.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MarcoGuidara\QuickResponseCache\Middlewares;
+
+use Closure;
+use Illuminate\Http\Request;
+use MarcoGuidara\QuickResponseCache\QuickResponseCache;
+use Spatie\ResponseCache\Middlewares\CacheResponse;
+
+class QuickCacheResponse
+{
+
+    protected CacheResponse $cache_response;
+
+    public function __construct(CacheResponse $cache_response)
+    {
+        $this->cache_response = $cache_response;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!QuickResponseCache::isEnabled()) {
+            return $next($request);
+        }
+
+        return $this->cache_response->handle($request, $next);
+    }
+}
